d91f0b9ab76b0cdfe9fd1573f97d3c020b7d1901
thaiwoy99 <71099139+thaiwoy99@users.noreply.github.com>
2023-04-09 14:30:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const icon = document.querySelector(".hamburger-btn");
+const ico = document.querySelector(".navs")
+
+console.log(ico)
+icon.addEventListener("click",function(){
+    document.body.classList.toggle("nav-open")
+})
+
+
+
+
